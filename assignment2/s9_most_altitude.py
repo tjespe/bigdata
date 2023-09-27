d43118a494c283ceb3a96ac589f9a1b0d074a0cf
Tord Johan Espe <tordjohanespe@gmail.com>
2023-09-27 14:50:48 +0200

    First draft for most altitude list

--- assignment2/s9_most_altitude.py
+++ assignment2/s9_most_altitude.py
@@ -1,22 +1,43 @@
-from string import printable
-from assignment2.DbConnector import DbConnector
+from DbConnector import DbConnector
+from helpers import print_table
 
 
 def get_users_with_most_altitude_gain():
     connection = DbConnector()
-    db_connection = connection.db_connection
     cursor = connection.cursor
     
     query = """
-        SELECT user_id, SUM(altitude_gain)
-        FROM Activity
-        GROUP BY user_id
-        ORDER BY SUM(altitude_gain)
-        DESC
+        WITH LaggedTrackPointData AS (
+            SELECT 
+                tp.activity_id,
+                tp.altitude,
+                LAG(tp.altitude, 1, 0) OVER (PARTITION BY tp.activity_id ORDER BY tp.date_days) AS prev_altitude
+            FROM 
+                TrackPoint tp
+        )
+
+        SELECT
+            a.user_id,
+            SUM(
+                CASE 
+                    WHEN l.altitude > l.prev_altitude THEN l.altitude - l.prev_altitude
+                    ELSE 0
+                END
+            ) as alt_gain_sum
+        FROM 
+            LaggedTrackPointData l
+        JOIN 
+            Activity a ON a.id = l.activity_id
+        GROUP BY
+            a.user_id
+        ORDER BY alt_gain_sum DESC
+        LIMIT 15
     """
     cursor.execute(query)
     rows = cursor.fetchall()
     print("Users with most altitude gain:")
-    printable(rows)
+    print_table(rows, ["User ID", "Altitude Gain Sum"])
     return rows
 
+if __name__ == "__main__":
+    get_users_with_most_altitude_gain()
