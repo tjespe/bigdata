8931f43d52679717ec349d250dc46cd0e922d4da
Tord Johan Espe <tordjohanespe@gmail.com>
2023-10-03 12:26:41 +0200

    Fix issues in s8

--- assignment2/s8_users_close_in_time_and_space_v2.py
+++ assignment2/s8_users_close_in_time_and_space_v2.py
@@ -3,11 +3,9 @@ import numpy as np
 
 import pandas as pd
 from DbConnector import DbConnector
-from helpers import haversine_np, print_table
-from haversine import haversine, Unit
+from helpers import haversine_np
 from math import ceil
 from sklearn.cluster import DBSCAN
-from datetime import datetime
 
 
 # Find the number of users which have been close to each other in time and space.
@@ -24,16 +22,16 @@ def users_close_in_time_and_space():
     users_with_meetings = set()
 
     min_euclidian_radial = 0
-    iteration = 0
+    page = 0
     start_time = pd.Timestamp.now()
+    prev_result = None
     while True:
         print(
-            f"\nIteration {iteration}/{ceil(9.6e6/POINTS_PER_QUERY)}, "
+            f" Page {page} of (estimated) {ceil(9681756/POINTS_PER_QUERY)}, "
             f"min_euclidian_radial = {min_euclidian_radial}, "
-            f"Elapsed time: {(pd.Timestamp.now()-start_time)/pd.Timedelta(seconds=1):.2f} seconds"
+            f"Elapsed time: {(pd.Timestamp.now()-start_time)/pd.Timedelta(seconds=1):.2f} seconds",
+            end="\r"
         )
-        print("Fetching data...", end="\r")
-        t = datetime.now()
         query = f"""
             SELECT user_id, activity_id, lat, lon, date_days, altitude, euclidian_radial
             FROM TrackPoint
@@ -43,62 +41,63 @@ def users_close_in_time_and_space():
             LIMIT {POINTS_PER_QUERY}
         """
         cursor.execute(query)
-        result = pd.DataFrame(
+        raw_result = pd.DataFrame(
             cursor.fetchall(),
             columns=["user_id", "activity_id", "lat", "lon", "date_days", "altitude", "euclidian_radial"]
         )
-        print("Fetched data. Elapsed time: ", datetime.now()-t, "ms")
 
-        if len(result) == 0:
+        if raw_result.empty:
+            print("\Empty result. Stopping.")
             break
 
-        num_rows = result.shape[0]
+        if prev_result is not None and prev_result.shape == raw_result.shape and (prev_result == raw_result).all().all():
+            # This can happen because the min_euclidian_radial we set for the next
+            # query is defined as the max of this minus sqrt(2), so for the last
+            # chunk of data, we are likely to fetch it again and again.
+            print("\nResult is the same as previous iteration. Stopping.")
+            break
+
+        prev_result = raw_result.copy()
+
+        num_rows = raw_result.shape[0]
 
         # Create x and y columns containing meters based on lat and lon
-        print("Preprocessing...", end="\r")
-        t = datetime.now()
-        result["y"] = haversine_np(np.zeros(num_rows), result["lat"], np.zeros(num_rows), np.zeros(num_rows))
-        result["y"] = result["y"]*np.sign(result["lat"])
-        result["x"] = haversine_np(result["lon"], result["lat"], np.zeros(num_rows), result["lat"])
-        result["x"] = result["x"]*np.sign(result["lon"])
+        processed_df = raw_result.copy()
+        processed_df["y"] = haversine_np(np.zeros(num_rows), processed_df["lat"], np.zeros(num_rows), np.zeros(num_rows))
+        processed_df["y"] = processed_df["y"]*np.sign(processed_df["lat"])
+        processed_df["x"] = haversine_np(processed_df["lon"], processed_df["lat"], np.zeros(num_rows), processed_df["lat"])
+        processed_df["x"] = processed_df["x"]*np.sign(processed_df["lon"])
 
         # Scale x, y and time so that the distance limits equal 1
-        result["x_scaled"] = result["x"] / 50
-        result["y_scaled"] = result["y"] / 50
-        result["time_scaled"] = result["date_days"]*24*60*60 / 30
-        print("Preprocessing done. Elapsed time: ", datetime.now()-t, "ms")
+        processed_df["x_scaled"] = processed_df["x"] / 50
+        processed_df["y_scaled"] = processed_df["y"] / 50
+        processed_df["time_scaled"] = processed_df["date_days"]*24*60*60 / 30
 
         # Apply DBSCAN clustering
-        print("Applying DBSCAN...", end="\r")
-        t = datetime.now()
         epsilon = 1
         db = DBSCAN(eps=epsilon, min_samples=1, metric='l2').fit(
-            result[["x_scaled", "y_scaled", "time_scaled"]]
+            processed_df[["x_scaled", "y_scaled", "time_scaled"]]
         )
-        print("DBSCAN done. Elapsed time: ", datetime.now()-t, "ms")
         labels = db.labels_
-        result["cluster"] = labels
+        processed_df["cluster"] = labels
 
         # Discard clusters that only have 1 unique user_id
-        print("Discarding irrelevant clusters")
-        activities_per_cluster = result.groupby("cluster")["user_id"].nunique()
+        activities_per_cluster = processed_df.groupby("cluster")["user_id"].nunique()
         relevant_clusters = activities_per_cluster[activities_per_cluster > 1].index
-        result = result[result["cluster"].isin(relevant_clusters)]
+        filtered_result = processed_df[processed_df["cluster"].isin(relevant_clusters)]
         
-        print("Updating users_with_meetings")
-        users_with_meetings.update(set(result["user_id"]))
+        users_with_meetings.update(set(filtered_result["user_id"]))
 
         # Define the next minimum euclidian radial. We must subtract sqrt(2)
         # because we need some overlap, in case the last trackpoint in this
         # query is close to the first trackpoint in the next query.
-        min_euclidian_radial = result["euclidian_radial"].max(skipna=True) - np.sqrt(2)
+        min_euclidian_radial = raw_result["euclidian_radial"].max(skipna=True) - np.sqrt(2)
         if np.isnan(min_euclidian_radial):
-            print(min_euclidian_radial,"is nan!")
-        iteration += 1
+            raise Exception("min_euclidian_radial is nan!")
+        page += 1
 
     if users_with_meetings:
-        columns = ["Number of distinct close users"]
-        print_table(len(users_with_meetings), columns)
+        print("Number of distinct close users:", len(users_with_meetings))
     else:
         print("No data found")
     
