94b06988c7b4e85a10e8bb014147b77ab5785384
Kristoffer Grude <kristoffernbg@gmail.com>
2023-10-21 12:27:37 +0200

    queries to ex 1,4,7

---
+++ assignment3/s1_num_users_activities_trackpoints.py
@@ -0,0 +1,31 @@
+from DbConnector import DbConnector
+
+# How many users, activities and trackpoints are there in the dataset?
+
+def num_users_activites_trackpoints():
+    # Connect to db
+    connection = DbConnector()
+    client = connection.client
+    db = connection.db
+
+    # Query
+    activity_count = db["Activity"].count_documents({})
+    user_count = len(db["Activity"].distinct("user_id"))
+    
+    pipeline = [
+        {"$project": {"trackpoint_count": {"$size": "$trackpoints"}}},
+        {"$group": {"_id": None, "trackpoint_count": {"$sum": "$trackpoint_count"}}}
+
+    ]
+    trackpoint_count = db["Activity"].aggregate(pipeline).next()["trackpoint_count"]
+
+    
+    # Print
+    print(f"Number of users: {user_count}")
+    print(f"Number of activities: {activity_count}")
+    print(f"Number of trackpoints: {trackpoint_count}")
+
+    client.close()
+
+if __name__ == "__main__":
+    num_users_activites_trackpoints()
