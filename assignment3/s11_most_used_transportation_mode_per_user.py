a163f4945db28baf4a354e96838655429b38e2ed
Sivert Eggen <sivertae@stud.ntnu.no>
2023-10-21 13:18:13 +0200

    Query 11

---
+++ assignment3/s11_most_used_transportation_mode_per_user.py
@@ -0,0 +1,37 @@
+# Find all users who have registered transportation_mode and their most used
+# transportation_mode
+
+from DbConnector import DbConnector
+
+
+def most_used_transportation_mode_per_user():
+    connection = DbConnector()
+    client = connection.client
+    db = connection.db
+
+
+    # creating query
+    query = db.activities.aggregate([
+        {
+            "$match": {
+                "transportation_mode": {"$ne": None}
+            }
+        },
+        {
+            "$group": {
+                "_id": {"userid": "$userid", "transportation_mode": "$transportation_mode"},
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$group": {
+                "_id": "$id.userid",
+                "transportation_mode": {"$first": "$_id.transportation_mode"},
+            }
+        }
+    ])
+
+    print(query)
