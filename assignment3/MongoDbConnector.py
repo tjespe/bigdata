3fe0c41ae0d1a86d04ff1e3cbe74deab9faa43e5
Tord Johan Espe <tordjohanespe@gmail.com>
2023-10-26 13:48:33 +0200

    Remove unnecessary prints

--- assignment3/MongoDbConnector.py
+++ assignment3/MongoDbConnector.py
@@ -30,13 +30,7 @@ class DbConnector:
         except Exception as e:
             print("ERROR: Failed to connect to db:", e)
 
-        # get database information
-        print("You are connected to the database:", self.db.name)
-        print("-----------------------------------------------\n")
-
     def close_connection(self):
         # close the cursor
         # close the DB connection
         self.client.close()
-        print("\n-----------------------------------------------")
-        print("Connection to %s-db is closed" % self.db.name)
