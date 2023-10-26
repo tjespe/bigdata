a4fbb075f57ab1e6bde2b83d4131b6c0b5d50e09
Tord Johan Espe <tordjohanespe@gmail.com>
2023-10-26 14:21:26 +0200

    Fix q 9

--- assignment2/s11_users_with_invalid_activites.py
+++ assignment2/s11_users_with_invalid_activites.py
@@ -1,8 +1,9 @@
 from DbConnector import DbConnector
 from helpers import print_table
 
-# Find all users with invalid activities, and the number of invalid activities per user 
+# Find all users with invalid activities, and the number of invalid activities per user
 # An invalid activity is defined as an activity with consecutive trackpoints where the timestamps deviate with at least 5 minutes.
+
 
 def users_with_invalid_activites():
     connection = DbConnector()
@@ -16,18 +17,20 @@ def users_with_invalid_activites():
         GROUP BY user_id
         ORDER BY num_invalid_activites DESC
     """
-    
+
     cursor.execute(query)
     result = cursor.fetchall()
 
     if result:
         columns = ["User ID", "Number of Invalid Activites"]
+        print(len(result), "users have invalid activities")
         print_table(result, columns)
     else:
         print("No data found")
-    
+
     db_connection.close()
     connection.close_connection()
+
 
 if __name__ == "__main__":
     users_with_invalid_activites()
--- assignment3/s9_users_with_invalid_activites.py
+++ assignment3/s9_users_with_invalid_activites.py
@@ -15,17 +15,31 @@ def users_with_invalid_activites():
     pipeline = [
         {"$unwind": "$trackpoints"},
         {"$match": {"trackpoints.minutes_diff": {"$gte": 5}}},
-        {"$group": {"_id": "$user_id", "num_invalid_activities": {"$sum": 1}}},
-        {"$sort": {"num_invalid_activities": -1}},
+        # Count unique activity IDs per user
+        {
+            "$group": {
+                "_id": "$user_id",
+                "num_invalid_activites": {"$addToSet": "$_id"},
+            }
+        },
+        # Count number of unique activity IDs per user
+        {
+            "$project": {
+                "_id": 0,
+                "user_id": "$_id",
+                "num_invalid_activites": {"$size": "$num_invalid_activites"},
+            }
+        },
+        {"$sort": {"num_invalid_activites": -1}},
     ]
 
     output = db["activities"].aggregate(pipeline)
-
-    users = [[user["_id"]] for user in output]
+    users = [[user["user_id"], user["num_invalid_activites"]] for user in output]
 
     # Print result
     if users:
         columns = ["User ID", "Number of Invalid Activites"]
+        print(len(users), "users have invalid activities")
         print_table(users, columns)
     else:
         print("No data found")
